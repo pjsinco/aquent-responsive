726dae2c8ef9f6d162022076b96694b084cc6c6f
Patrick Sinco <psinco@gmail.com>
2014-10-31 07:10:36 -0500

    continue to mark up and style index.html

--- config.rb
+++ config.rb
@@ -2,6 +2,7 @@ require 'compass-normalize'
 http_path = '/'
 css_dir = '_/css'
 sass_dir = '_/components/sass'
+fonts_dir = 'fonts'
 javascripts_dir = '_/js'
 output_style = :nested
 images_dir = 'images'
